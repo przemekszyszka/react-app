a10d78fc0218ac961b63c69726095026783ad92b
Przemysław Szyszka <przemeklo@o2.pl>
2015-05-13 16:45:04 +0200

    #3 - Add score to story; Install jquery depedency

--- src/story.jsx
+++ src/story.jsx
@@ -9,6 +9,7 @@ class Story extends React.Component {
     return (
       <div>
         <a href={this.props.story.url}>{this.props.story.title}</a>
+        <span>scrore: {this.props.story.score}</span>
       </div>
     );
   }
