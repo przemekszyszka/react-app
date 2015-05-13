27777058cc41ee2aeaeed00c86de480d2b8ae3a6
Przemysław Szyszka <przemeklo@o2.pl>
2015-05-13 13:50:32 +0200

    #3 - Use stories list with mocked data

--- src/hello_world.jsx
+++ src/hello_world.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import StoriesList from './stories_list'
 
 class HelloWorld extends React.Component {
   render() {
-    return <p> Hello, world! </p>;
+    var stories=[{ id: "1", title: "XXX", url: "http://www.google.com" }, { id: "2", title: "XXX", url: "http://www.google.com" }]
+
+    return (
+      <div>
+        <p> Hello, world! </p>
+        <div>Stories:</div>
+        <StoriesList stories={stories} />
+      </div>
+    );
   }
 }
 
