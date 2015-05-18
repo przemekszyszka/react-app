322d7db2c7c4198bb74fe948540f96e949a268c8
Przemysław Szyszka <przemeklo@o2.pl>
2015-05-18 12:08:27 +0200

    #5 - Implement route handler in menu component. Remove page component

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,23 +1,37 @@
 import React from 'react';
 import {Nav, NavItem} from 'react-bootstrap';
+import {RouteHandler} from "react-router";
 
 class Menu extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {activeKey: 1};
+    this.state = location.href.indexOf("recent") != -1 ? {activeKey: 2} : {activeKey: 1}
   }
 
   render() {
     return (
-      <Nav bsStyle='pills' activeKey={this.state.activeKey} onSelect={this._handleSelect.bind(this)}>
-        <NavItem eventKey={1} href='/home'>Popular</NavItem>
-        <NavItem eventKey={2} href='/home'>Recent</NavItem>
-      </Nav>
+      <div className='row'>
+        <div className='col-sm-10 col-sm-offset-1'>
+          <Nav bsStyle='pills' activeKey={this.state.activeKey} onSelect={this._handleSelect.bind(this)}>
+            <NavItem eventKey={1} href='#/popular'>Popular stories</NavItem>
+            <NavItem eventKey={2} href='#/recent'>Recent stories</NavItem>
+          </Nav>
+          <RouteHandler />
+        </div>
+      </div>
     );
   }
 
   _handleSelect(selectedKey) {
     this.setState({activeKey: selectedKey});
+    switch (selectedKey) {
+      case 1:
+        location.href = '#/popular'
+        return;
+      case 2:
+        location.href = '#/recent'
+        break;
+    }
   }
 }
 
